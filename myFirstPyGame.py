5d821a2dc82c4a42d41f8260d6324254044325bd
Jahaun G <s11088077@students.duvalschools.org>
2021-12-09 14:42:55 -0500

    v0.7
    Added circle, ellipse, and rectangle

--- myFirstPyGame.py
+++ myFirstPyGame.py
@@ -1,4 +1,4 @@
-# My First Game, Jahaun Gilmore, 12/9/21 2:27pm, v0.6
+# My First Game, Jahaun Gilmore, 12/9/21 2:42pm, v0.7
 
 import pygame, sys
 from pygame.locals import *
@@ -32,7 +32,16 @@ windowSurface.fill(WHITE)
 # Draw a polygon onto the screen.
 pygame.draw.polygon(windowSurface, BLUE, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
 
-#Draw a polygon onto the screen.
+# Draw a polygon onto the screen.
 pygame.draw.line(windowsSurface, GREEN, (60,60), (120,60), 4)
 pygame.draw.line(windowsSurface, BLUE, (256,300), (300,400), 3)
-pygame.draw.line(windowsSurface, NOTRED, (100,200), (140,235), 2)
+pygame.draw.line(windowsSurface, NOTRED, (100,200), (140, 235), 2)
+
+# Draw a circle
+pygame.draw.circle(windowSurface, RED, (300, 50), 20, 0)
+
+# Draw an ellipse
+pygame.draw.ellipse(windowSurface, NOTRED, (300, 250, 40, 80,), 1)
+
+#Draw the text rectangle
+pygame.draw.rect(windowSurface, BLACK, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40)
