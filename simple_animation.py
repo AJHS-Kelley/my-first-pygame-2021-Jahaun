9a6c2c621470a8ed545ee6b6d8e0533c6913712b
Jahaun <s11088077@students.duvalschools.org>
2022-02-02 14:34:37 -0500

    v0.2
    Established movement for DIAGONAL movement
    
    Established the MOVESPEED >

--- simple_animation.py
+++ simple_animation.py
@@ -1,4 +1,4 @@
-# Simple Animation with PyGame, Jahaun Gilmore, 2/2/22, 2:26PM, v0.1
+# Simple Animation with PyGame, Jahaun Gilmore, 2/2/22, 2:32PM, v0.2
 
 import pygame, sys, time
 from pygame.locals import *
@@ -11,3 +11,11 @@ WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
 windowSurface = pygame.display.set_mode(WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Animation Example!') 
+
+# Setup the direction variables.
+DOWNLEFT = 'downleft'
+DOWNRIGHT = 'downright'
+UPLEFT = 'upleft'
+UPRIGHT = 'upright'
+
+MOVESPEED = 4
